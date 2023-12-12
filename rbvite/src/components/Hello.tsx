79679ea0f4582ed5c30209aedb79851e18c1f68a
unknown <pqpqpq460@gmail.com>
2023-12-12 22:37:46 +0900

    Revert changes in Hello.tsx.
    This reverts commit 6e4b7133522fd76e0731200b74bfc5f685386f20.

--- rbvite/src/components/Hello.tsx
+++ rbvite/src/components/Hello.tsx
@@ -1,27 +1,15 @@
-// import { PropsWithChildren } from 'react';
-import { ReactElement } from 'react';
+import { PropsWithChildren } from 'react';
 
 type Props = {
   name: string;
   age: number;
-  children: ReactElement;
 };
 
-// const Hello = ({ name, age, children }: PropsWithChildren<Props>) => {
-const Hello = (prop: Props) => {
-  let { name, age, children } = prop;
-
-  // prop.age 변경하면 화면이 아예 그려지지 않는다. (not primitive)
-  prop.age = prop.age + 1;
-  age = age + 1; // (primitive)
-
-  console.log('Hello.age>>', age);
-  console.log('props.age>>', prop.age);
-
+const Hello = ({ name, age, children }: PropsWithChildren<Props>) => {
   return (
     <>
       <h2>
-        Hello, {name} ({age}세? {prop.age}세?)
+        Hello, {name} ({age}세)
       </h2>
       <h3>{children}</h3>
     </>
