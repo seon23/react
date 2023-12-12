2c4167a4b40fa3668584a274d71078acd49af4da
unknown <pqpqpq460@gmail.com>
2023-12-12 23:26:04 +0900

    Update 'index.css' and 'App.css'

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css';
+import Box from './components/Box';
 import Title from './components/TItle';
 
 function App() {
   return (
     <>
-      <Title title='React Tutorial' color='red'>
-        sub title: react basic
-      </Title>
+      <Box
+        borderWidth='2px'
+        borderColor='blue'
+        borderStyle='solid'
+        padding='4px'
+        margin='2px'
+      >
+        <Title title='React Tutorial' color='red'>
+          sub title: react basic
+        </Title>
+      </Box>
     </>
   );
 }
