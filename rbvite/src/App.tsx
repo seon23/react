338fd46c57627dc7a48fe72916715c6ff8136b44
unknown <pqpqpq460@gmail.com>
2023-12-12 20:51:58 +0900

    Pass Props to a Component.

--- rbvite/src/App.tsx
+++ rbvite/src/App.tsx
@@ -4,13 +4,18 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
+  const h1Style = { backgroundColor: 'grey', color: 'white' };
+  const Title = ({ txt }: { txt: string }) => <h1 style={h1Style}>{txt}</h1>;
+
   return (
     <>
-      <h1>Vite + React</h1>
+      {count && <Title txt={`Vite + React ${count}`} />}
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count > 0 ? 'Big' : 'Zero'}
         </button>
+        <p>Count: {count && 'Non-Zero'}</p>
+        <input type='text' />
       </div>
     </>
   );
