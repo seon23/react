73f1c25a8a4da3fb7306a3e8448b8c3b0dd8cf2c
unknown <pqpqpq460@gmail.com>
2023-12-13 00:26:33 +0900

    Use React.memo to prevent re-renders of Title component.

--- practice/src/components/TItle.tsx
+++ practice/src/components/TItle.tsx
@@ -18,23 +18,3 @@ const Title = ({ title, color, children }: PropsWithChildren<Props>) => {
 };
 
 export default memo(Title);
-// Usage
-
-// 1.부모 컴포넌트의 리렌더링과 관계없이 props가 변경되었을 때만 리렌더링!
-// const SomeComponent = ({ name, age }) => { . . . };
-
-// export default memo(SomeComponent);
-
-// 2.props 중 name이 변경되었을 때만 리렌더링!
-// const SomeComponent = ({ name, age }) => { . . . };
-// export default memo(SomeComponent,
-//   ({ name: prevName }, { name: currName }) =>
-//      prevName === currName
-
-// 3.항상 캐시된 컴포넌트 사용(리렌더링X)
-//   const SomeComponent = ({ name, age }) => {
-//   . . .
-// };
-// export default memo(SomeComponent,
-//   () => true
-// );
