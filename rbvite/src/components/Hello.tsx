916a698c0b26af1cfe38150dbfd2c7165658cecc
unknown <pqpqpq460@gmail.com>
2023-12-14 17:47:18 +0900

    Update App.tsx and Hello.tsx for new page.

--- rbvite/src/components/Hello.tsx
+++ rbvite/src/components/Hello.tsx
@@ -3,15 +3,22 @@ import { PropsWithChildren } from 'react';
 type Props = {
   name: string;
   age: number;
+  incrementCount: () => void;
 };
 
-const Hello = ({ name, age, children }: PropsWithChildren<Props>) => {
+const Hello = ({
+  name,
+  age,
+  incrementCount,
+  children,
+}: PropsWithChildren<Props>) => {
   return (
     <>
       <h2>
         Hello, {name} ({age}세)
       </h2>
       <h3>{children}</h3>
+      <button onClick={incrementCount}>count + 1</button>
     </>
   );
 };
